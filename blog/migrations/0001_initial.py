b8f2d0116e53a7b1137c6d5ed337dc114860a472
David Cotter <daveguy28@gmail.com>
2024-05-22 09:52:30 +0000

    add the coding

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -17,13 +17,16 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200, unique=True)),
                 ('slug', models.SlugField(max_length=200, unique=True)),
                 ('content', models.TextField()),
                 ('created_on', models.DateTimeField(auto_now_add=True)),
-                ('status', models.IntegerField(choices=[(0, 'Draft'), (1, 'Published')], default=0)),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='blog_posts', to=settings.AUTH_USER_MODEL)),
+                ('status', models.IntegerField(choices=[
+                 (0, 'Draft'), (1, 'Published')], default=0)),
+                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                 related_name='blog_posts', to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
 
-class Ticket(models.Model):
-    ticket_holder = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="users_tickets"
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    date_issued = models.DateTimeField(auto_now_add=True)
-    event = models.ForeignKey(
-        Event,
-        on_delete=models.CASCADE,
-        related_name="event_tickets"
-    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
 
     def __str__(self):
-        return f"Ticket for {self.ticket_holder}"
+        return f"{self.title} | written by {self.author}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
