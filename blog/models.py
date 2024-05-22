3b5580105a3b87b8cf7e4b7e13b4f15869191661
David Cotter <daveguy28@gmail.com>
2024-05-22 08:32:37 +0000

    remove code

--- blog/models.py
+++ blog/models.py
@@ -3,15 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
-class Event(models.Model):
-    event_name = models.CharField(max_length=200, unique=True)
-    location = models.CharField(max_length=200)
-    date = models.DateTimeField()
-
-    def __str__(self):
-        return self.event_name
-
 class Ticket(models.Model):
     ticket_holder = models.ForeignKey(
         User,
