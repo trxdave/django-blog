6f63f584bbf8cb79550345ed111a53f1c0312a7f
David Cotter <daveguy28@gmail.com>
2024-05-22 08:02:15 +0000

    Model relationships

--- blog/models.py
+++ blog/models.py
@@ -2,14 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-STATUS = ((0, "Draft"), (1, "Published"))
 
-class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=200, unique=True)
+    location = models.CharField(max_length=200)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.event_name
+
+class Ticket(models.Model):
+    ticket_holder = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="users_tickets"
     )
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    date_issued = models.DateTimeField(auto_now_add=True)
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        related_name="event_tickets"
+    )
+
+    def __str__(self):
+        return f"Ticket for {self.ticket_holder}"
