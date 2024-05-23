131ca9c55081bf611e978e04e0a33f0c943c8b30
David Cotter <daveguy28@gmail.com>
2024-05-23 11:06:51 +0000

    save the files

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q^q^0mqsyjc@t_rcr2_p4*@sige%55&1crg9oz57vwbj@wojj+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-trxdave-djangoblog-jdbgyrjry59.ws-eu111.gitpod.io', '.herokuapp.com']
--- codestar/urls.py
+++ codestar/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("", include("blog.urls"), name="blog-urls"),
+    path("blog/", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
 ]
