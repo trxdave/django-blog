aa9fe42b35d1ddd4d3d99bb5b6ee7094914d10e9
David Cotter <daveguy28@gmail.com>
2024-05-28 12:51:07 +0000

    views and urls update

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,6 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
-
-
 class PostList(generic.ListView):
     queryset = Post.objects.all().order_by("-created_on")
-    # template_name = "post_list.html"
-
-
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
--- codestar/urls.py
+++ codestar/urls.py
@@ -17,9 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
 ]
