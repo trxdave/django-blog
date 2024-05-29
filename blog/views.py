d8c4dff76cd1ad0c129fd752f05f432a605e53f5
David Cotter <daveguy28@gmail.com>
2024-05-29 10:17:07 +0000

    index and base

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
+
+
 class PostList(generic.ListView):
-    queryset = Post.objects.all().order_by("-created_on")
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
