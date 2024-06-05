f799357d2565e2fa032724325b421c765bd55106
David Cotter <daveguy28@gmail.com>
2024-06-05 11:06:58 +0000

    about

--- about/views.py
+++ about/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import About
+
+
+def about_view(request):
+    about_instance = About.objects.first()
+    return render(request, 'about/about.html', {'about': about_instance})
