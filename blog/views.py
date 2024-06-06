9d7d1c706e843c756c495daa766f8c64ee1ec48d
David Cotter <daveguy28@gmail.com>
2024-06-06 12:33:36 +0000

    Create the Collaboration Request form

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
@@ -29,7 +30,20 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+
     comment_form = CommentForm()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+        request, messages.SUCCESS,
+        'Comment submitted and awaiting approval'
+    )
 
     return render(
         request,
@@ -37,7 +51,7 @@ def post_detail(request, slug):
         {"post": post,
          "comments": comments,
          "comment_count": comment_count,
-         "coder": "David Cotter"
+         "coder": "David Cotter",
          "comment_form": comment_form,
          },
     )
